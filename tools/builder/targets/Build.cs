ead528d92a4571a9ebea8dedfae705c3c8884789
Brad Wilson <dotnetguy@gmail.com>
2026-03-12 10:08:28 -0700

    xunit/xunit#3328: Copy .md files into the .site result

--- tools/builder/targets/Build.cs
+++ tools/builder/targets/Build.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using Xunit.BuildTools.Models;
 
@@ -14,5 +16,40 @@ public static class Build
 		context.BuildStep("Building web site");
 
 		await context.Exec("dotnet", "docfx site/docfx.json");
+
+		var srcFolder = Path.Combine(context.BaseFolder, "site");
+		var dstFolder = Path.Combine(context.BaseFolder, ".site");
+		var anyCopied = false;
+
+		foreach (var srcFile in Directory.GetFiles(srcFolder, "*.md", SearchOption.AllDirectories))
+		{
+			var dstFile = srcFile.Replace(srcFolder, dstFolder);
+
+			if (srcFile != dstFile)
+			{
+				var shouldCopy = false;
+
+				if (!File.Exists(dstFile))
+					shouldCopy = true;
+				else
+				{
+					var srcDate = File.GetLastWriteTimeUtc(srcFile);
+					var dstDate = File.GetLastWriteTimeUtc(dstFile);
+
+					shouldCopy = dstDate < srcDate;
+				}
+
+				if (shouldCopy)
+				{
+					anyCopied = true;
+
+					context.WriteLineColor(ConsoleColor.DarkGray, $"COPY: {srcFile[(context.BaseFolder.Length + 6)..]}");
+					File.Copy(srcFile, dstFile);
+				}
+			}
+		}
+
+		if (anyCopied)
+			Console.WriteLine();
 	}
 }
